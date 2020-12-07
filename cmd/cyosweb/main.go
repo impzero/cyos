dd7553e6b878bfc459048e4930f19f6768939667
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-12-07 19:22:37 +0200

    wip

--- cmd/cyosweb/main.go
+++ cmd/cyosweb/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/impzero/cyos"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	port := flag.Int("port", 8080, "The port to run the web server on")
+	filename := flag.String("file", "story.json", "The file that contains the story structure in JSON")
+	flag.Parse()
+	fmt.Printf("Using the story in %s!\n", *filename)
+
+	// Open the file
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyos.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyos.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	fmt.Printf("%+v\n", story)
 }
